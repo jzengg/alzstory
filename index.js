d4aea8662ded44db55f09dd9579d552ccc3a2203
Jeffrey King <nerdpride4life@gmail.com>
2016-06-04 17:13:57 -0400

    Rework index.html

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
-profiles = {
-	"currentProfile" : "",
-	"profiles" :
-	[
-		{
-			"name" : "Mike",
-			"age" : "57",
-			"diseaseStage" : "Early Onstage",
-			"location" : "Boston, MA",
-			"caregiverPatient" : "Patient",
-			"username" : "mikeisgreat",
-			"diagnosisYear" : "2013",
-			"videoURL" : "",
-			"photoURL" : "./images/avatar.png",
-			"archiveVideos" : [],
-			"contact" :
-			[
-				{
-					"email" : "mike@alz.org",
-					"public" : true
-				},
-				{
-					"skype" : "mikeIsGreat",
-					"public" : true
-				},
-				{
-					"hangout" : "mike",
-					"public" : true
-				},
-				{
-					"phone" : "609-502-2132",
-					"public" : false
-				}
-			],
-			"hobbies" : ["basketball", "technology", "videogames"],
-			"friends" : ["Steve", "Sheryl", "Judy"],
-		},
-	]
-};
-
-var profileController = {
-
-};
-
-var viewProfile = {
-	init: function() {
-		// Grab all the dom elements
-		this.profileImage = $(".profile_image");
-		this.profileVideo = $(".profile_video");
-		this.bio = $(".bio");
-		this.hobbies = $(".hobbies");
-		this.friends = $(".freinds");
-	},
-	render: function() {
-		
-	}
-};
+$( document ).ready(function() {
+	alert("hey!");
+    $(".archiveDropdown").click(function() {
+    	alert("Does this work?");
+    	$(".archiveVideos").removeClass(".hidden");
+    });
+});
