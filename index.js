64269f45c0194d4c6e5efc35cc7ab37aec7ff3ec
Jeffrey King <nerdpride4life@gmail.com>
2016-06-04 15:04:14 -0400

    Add JSON for profile page

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-profiles =
-[
-	{
-		"name" : "Mike",
-		"age" : "",
-		"diseaseStage" : "",
-		"location" : "",
-		"caregiverPatient" : "",
-		"username" : "",
-		"diagnosisYear" : "",
-		"videoURL" : "",
-		"photoURL" : "",
-		"archiveVideos" : [],
-		"contact" :
-		[
-			{
-				"email" : "mike@alz.org",
-				"public" : true
-			},
-			{
-				"skype" : "mikeIsGreat",
-				"public" : true
-			},
-			{
-				"hangout" : "mike",
-				"public" : true
-			},
-			{
-				"phone" : "609-502-2132",
-				"public" : false
-			}
-		],
-		"hobbies" : ["basketball", "technology", "videogames"],
-		"friends" : ["Steve", "Sheryl", "Judy"],
-	},
+profiles = {
+	"currentProfile" : "",
+	"profiles" :
+	[
+		{
+			"name" : "Mike",
+			"age" : "",
+			"diseaseStage" : "",
+			"location" : "",
+			"caregiverPatient" : "",
+			"username" : "",
+			"diagnosisYear" : "",
+			"videoURL" : "",
+			"photoURL" : "",
+			"archiveVideos" : [],
+			"contact" :
+			[
+				{
+					"email" : "mike@alz.org",
+					"public" : true
+				},
+				{
+					"skype" : "mikeIsGreat",
+					"public" : true
+				},
+				{
+					"hangout" : "mike",
+					"public" : true
+				},
+				{
+					"phone" : "609-502-2132",
+					"public" : false
+				}
+			],
+			"hobbies" : ["basketball", "technology", "videogames"],
+			"friends" : ["Steve", "Sheryl", "Judy"],
+		},
+	]
+};
 
-];
+var profileController = {
+
+};
+
+var viewProfile = {
+	init: function() {
+		// Grab all the dom elements
+	},
+	render: function() {
+
+	}
+};
