9dbbf845942be8bdd39d585519d96ddf3d4b8994
Jeffrey King <nerdpride4life@gmail.com>
2016-06-04 14:57:25 -0400

    Add basic JSON data

---
+++ index.js
@@ -0,0 +1,37 @@
+profiles =
+[
+	{
+		"name" : "Mike",
+		"age" : "",
+		"diseaseStage" : "",
+		"location" : "",
+		"caregiverPatient" : "",
+		"username" : "",
+		"diagnosisYear" : "",
+		"videoURL" : "",
+		"photoURL" : "",
+		"archiveVideos" : [],
+		"contact" :
+		[
+			{
+				"email" : "mike@alz.org",
+				"public" : true
+			},
+			{
+				"skype" : "mikeIsGreat",
+				"public" : true
+			},
+			{
+				"hangout" : "mike",
+				"public" : true
+			},
+			{
+				"phone" : "609-502-2132",
+				"public" : false
+			}
+		],
+		"hobbies" : ["basketball", "technology", "videogames"],
+		"friends" : ["Steve", "Sheryl", "Judy"],
+	},
+
+];
